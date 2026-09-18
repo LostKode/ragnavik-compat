3f7d3a4efbcba9373de277196aac6f5cc75bb2d8
Daniel Cornelison <itsklastic@gmail.com>
2026-09-18 06:43:17 -0500

    chore(release): prepare compatibility 1.0.1 (#9)

--- src/RagnavikCompat/RagnavikCompatPlugin.cs
+++ src/RagnavikCompat/RagnavikCompatPlugin.cs
@@ -9,7 +9,7 @@ using UnityEngine.Rendering;
 
 namespace RagnavikCompat;
 
-[BepInPlugin(PluginGuid, "Ragnavik Compatibility", "1.0.0")]
+[BepInPlugin(PluginGuid, "Ragnavik Compatibility", "1.0.1")]
 [BepInDependency("WackyMole.EpicMMOSystem", BepInDependency.DependencyFlags.SoftDependency)]
 public sealed class RagnavikCompatPlugin : BaseUnityPlugin
 {
