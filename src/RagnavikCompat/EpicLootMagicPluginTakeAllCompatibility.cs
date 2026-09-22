c3293c0964665ef3d7475faf89aeb88da9431bc7
Daniel Cornelison <itsklastic@gmail.com>
2026-09-22 06:16:39 -0500

    fix: keep take all active across mod updates

--- src/RagnavikCompat/EpicLootMagicPluginTakeAllCompatibility.cs
+++ src/RagnavikCompat/EpicLootMagicPluginTakeAllCompatibility.cs
@@ -2,10 +2,6 @@ namespace RagnavikCompat;
 
 public static class EpicLootMagicPluginTakeAllCompatibility
 {
-    public static readonly System.Version SupportedEpicLootVersion = new(0, 14, 10);
-    public static readonly System.Version SupportedMagicPluginVersion = new(2, 2, 1);
-
     public static bool Supports(System.Version? epicLootVersion, System.Version? magicPluginVersion) =>
-        epicLootVersion == SupportedEpicLootVersion &&
-        magicPluginVersion == SupportedMagicPluginVersion;
+        epicLootVersion != null && magicPluginVersion != null;
 }
